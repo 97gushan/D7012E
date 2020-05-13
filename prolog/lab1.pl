c259e647c0cf59e4499d786ab206373cc04963d4
97gushan <97gustavh@gmail.com>
2020-05-13 08:40:08 +0200

    refactoring

--- prolog/lab1.pl
+++ prolog/lab1.pl
@@ -1,42 +1,42 @@
 % Gustav Hansson
 
-action(state(robot(r1, Items), S, B, P),
+action(state(robot(r1, OwnedItems), Items),
     move(r2), 
-    state(robot(r2, Items), S, B, P)) :-
-        member(steel, Items).
+    state(robot(r2, OwnedItems), Items)) :-
+        member(steel, OwnedItems).
 
-action(state(robot(r2, Items), S, B, P),
+action(state(robot(r2, OwnedItems), Items),
     move(r1), 
-    state(robot(r1, Items), S, B, P)) :-
-        member(steel, Items).
+    state(robot(r1, OwnedItems), Items)) :-
+        member(steel, OwnedItems).
 
-action(state(robot(r1, Items), S, B, P),
+action(state(robot(r1, OwnedItems), Items),
     move(r3), 
-    state(robot(r3, Items), S, B, P)) :-
-        member(brass, Items).
+    state(robot(r3, OwnedItems), Items)) :-
+        member(brass, OwnedItems).
 
-action(state(robot(r3, Items), S, B, P),
+action(state(robot(r3, OwnedItems), Items),
     move(r1), 
-    state(robot(r1, Items), S, B, P)) :-
-        member(brass, Items).
+    state(robot(r1, OwnedItems), Items)) :-
+        member(brass, OwnedItems).
 
-action(state(robot(Pos, Items), S, B, P),
+action(state(robot(Pos, OwnedItems), Items),
     pickup(Item),
-    state(robot(Pos, [Item | Items]), NS, NB, NP)) :-
-        member(item(Item, Pos), [S, B, P]),
-        length(Items, L),
+    state(robot(Pos, [Item | OwnedItems]), UpdatedItems)) :-
+        member(item(Item, Pos), Items),                                 % if the item exists in the current room
+        length(OwnedItems, L),                                          % check so that the robot only carries a max of 2 items
         L < 2,
-        select(item(Item, _), [S, B, P], item(Item, -1), [NS, NB, NP]).  
+        select(item(Item, _), Items, item(Item, -1), UpdatedItems).     %  update the position of the item to be outside any room
 
-action(state(robot(Pos, Items), S, B, P),
+action(state(robot(Pos, OwnedItems), Items),
     drop(Item),
-    state(robot(Pos, NewItems), NS, NB, NP)) :- 
-        member(Item, Items),
-        delete(Items, Item, NewItems),
-        select(item(Item, _), [S, B, P], item(Item, Pos), [NS, NB, NP]).  
+    state(robot(Pos, NewItems), UpdatedItems)) :- 
+        member(Item, OwnedItems),                                       % check so that the item that wants to be dropped is owned
+        delete(OwnedItems, Item, NewItems),                             % delete the item from the owned items and give a new list of owned items
+        select(item(Item, _), Items, item(Item, Pos), UpdatedItems).    % update the position of the dropped item to be the current position
 
-solverR(state(robot(r2, Items), _,_,_), _, [done | []]) :-
-    member(package, Items).
+solverR(state(robot(r2, OwnedItems), _), _, [done | []]) :-
+    member(package, OwnedItems).
 
 solverR(State, N, [Action | Trace]) :-
     N > 0,
@@ -44,9 +44,18 @@ solverR(State, N, [Action | Trace]) :-
     solverR(NewState, N-1, Trace).
 
 solve(Trace):-
-    BeginState = state(robot(r1, []), item(steel, r1), item(brass, r2), item(package, r3)),
+    BeginState = state(
+        robot(r1, []),
+        [item(steel, r1),
+        item(brass, r2),
+        item(package, r3)]),
+
     solverR(BeginState, 11, Trace).
 
 solve(Trace, N):-
-    BeginState = state(robot(r1, []), item(steel, r1), item(brass, r2), item(package, r3)),
+    BeginState = state(
+        robot(r1, []),
+        [item(steel, r1),
+        item(brass, r2),
+        item(package, r3)]),
     solverR(BeginState, N, Trace).
